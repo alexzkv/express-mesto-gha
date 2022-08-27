4513fc13e74efd0c5ca091723b14c6f7cd7c22df
AleksandrZakharov58 <91503552+AleksandrZakharov58@users.noreply.github.com>
2022-08-27 18:21:41 +0300

    feature: Add models

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // eslint-disable-next-line import/no-unresolved
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
