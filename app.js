bdca6b8f609843a935ec89a41772c8316d100fd3
AleksandrZakharov58 <91503552+AleksandrZakharov58@users.noreply.github.com>
2022-08-30 23:42:53 +0300

    feature: add function main

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
+const ERROR_SERVER = 500;
 
 app.use(express.json());
 app.use((req, res, next) => {
@@ -18,12 +18,17 @@ app.use((req, res, next) => {
 app.use('/', userRouter);
 app.use('/', cardRouter);
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: false,
-});
+async function main(req, res) {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/mestodb', {
+      useNewUrlParser: true,
+      useUnifiedTopology: false,
+    });
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`App listening on port ${PORT}`);
-});
+    await app.listen(PORT);
+  } catch (err) {
+    res.status(ERROR_SERVER).send({ message: 'Ошибка на сервере' });
+  }
+}
+
+main();
