2604a0d7be7ffd05f417729b466ed94653735e9e
AleksandrZakharov58 <91503552+AleksandrZakharov58@users.noreply.github.com>
2022-08-31 21:38:00 +0300

    refactor: delete status ok

--- controllers/cards.js
+++ controllers/cards.js
@@ -3,11 +3,10 @@ const Card = require('../models/card');
 const ERROR_BAD_REQUEST = 400;
 const ERROR_NOT_FOUND = 404;
 const ERROR_SERVER = 500;
-const STATUS_OK = 200;
 
 const getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.status(STATUS_OK).send({ cards }))
+    .then((cards) => res.send({ cards }))
     .catch(() => res.status(ERROR_SERVER).send({ message: 'Ошибка на сервере' }));
 };
 
@@ -15,7 +14,7 @@ const createCard = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(STATUS_OK).send({ card }))
+    .then((card) => res.send({ card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
@@ -30,7 +29,7 @@ const deleteCard = (req, res) => {
       if (!card) {
         return res.status(ERROR_NOT_FOUND).send({ message: 'Карточка не найдена' });
       }
-      return res.status(STATUS_OK).send({ card });
+      return res.send({ card });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -50,7 +49,7 @@ const likeCard = (req, res) => {
       if (!card) {
         return res.status(ERROR_NOT_FOUND).send({ message: 'Карточка не найдена' });
       }
-      return res.status(STATUS_OK).send({ card });
+      return res.send({ card });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -70,7 +69,7 @@ const dislikeCard = (req, res) => {
       if (!card) {
         return res.status(ERROR_NOT_FOUND).send({ message: 'Карточка не найдена' });
       }
-      return res.status(STATUS_OK).send({ card });
+      return res.send({ card });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
--- controllers/users.js
+++ controllers/users.js
@@ -3,11 +3,10 @@ const User = require('../models/user');
 const ERROR_BAD_REQUEST = 400;
 const ERROR_NOT_FOUND = 404;
 const ERROR_SERVER = 500;
-const STATUS_OK = 200;
 
 const getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.status(STATUS_OK).send({ data: users }))
+    .then((users) => res.send({ data: users }))
     .catch(() => res.status(ERROR_SERVER).send({ message: 'Ошибка на сервере' }));
 };
 
@@ -17,7 +16,7 @@ const getUserById = (req, res) => {
       if (!user) {
         return res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь не найден' });
       }
-      return res.status(STATUS_OK).send({ user });
+      return res.send({ user });
     })
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -31,7 +30,7 @@ const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.status(STATUS_OK).send({ data: user }))
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
@@ -47,7 +46,7 @@ const updateProfile = (req, res) => {
       if (!user) {
         return res.status(ERROR_SERVER).send({ message: 'Пользователь не найден' });
       }
-      return res.status(STATUS_OK).send({ user });
+      return res.send({ user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
@@ -64,7 +63,7 @@ const updateAvatar = (req, res) => {
       if (!user) {
         return res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь не найден' });
       }
-      return res.status(STATUS_OK).send({ user });
+      return res.send({ user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
