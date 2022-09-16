9cf362a5bec99b78821ec19b7aefbeea1d3a0059
Захаров Александр Александрович <AlekAleZakharov@vimpelcom.ru>
2022-09-16 10:15:23 +0300

    feat: add email, password

--- models/user.js
+++ models/user.js
@@ -1,19 +1,37 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    default: 'Жак',
     required: true,
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
+    default: 'Исследователь',
     required: true,
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
+    type: String,
+    default: 'https://yandex.ru',
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Введите корректный email',
+    },
+  },
+  password: {
     type: String,
     required: true,
   },
